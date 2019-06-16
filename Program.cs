500ff837262c127bb69670a6480ee5dde4551ef3
Michał Brzostek <michal.brzostek@gmail.com>
2019-06-16 23:20:58 +0200

    2.15

--- Program.cs
+++ Program.cs
@@ -11,13 +11,30 @@ namespace EntryCSharp
         static void Main(string[] args)
         {
             //String Lessons
-            string tekst;
-            Console.WriteLine("Please enter your name: ");
-            tekst = Console.ReadLine();
-            tekst = "Cześć " + tekst;
-            Console.WriteLine(tekst);
-            Console.WriteLine(tekst.Length);
+
+            //2.15
+            Console.WriteLine("Linia1\nLinia2s"); //przejście do nowej linii
+            Console.WriteLine("Wzrost 170\tWaga 80"); //t - tabulacja
+            Console.WriteLine(@"Wzrost 170\tWaga 80"); //@ - Dosłowna interpretacja
+            Console.WriteLine("C:\\System\\Windows"); //Podwójne \\ wstawia jedno \
+            Console.WriteLine("Tytuł filmu: \"Rój\""); //\" wstawia "
             Console.ReadLine();
+
+            //2.14
+            //string tekst;
+            //Console.WriteLine("Give a name:");
+            //tekst = Console.ReadLine();
+            //Console.WriteLine(tekst + 10);
+            //Console.ReadLine();
+
+            //2.13
+            //string tekst;
+            //Console.WriteLine("Please enter your name: ");
+            //tekst = Console.ReadLine();
+            //tekst = "Cześć " + tekst;
+            //Console.WriteLine(tekst);
+            //Console.WriteLine(tekst.Length);
+            //Console.ReadLine();
 
 
         }
